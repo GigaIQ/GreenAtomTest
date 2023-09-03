bd3aac72cb4ffbf0a5954ca93b267f16ed805db6
Arteom <92199229+GigaIQ@users.noreply.github.com>
2023-09-03 11:21:31 +0300

    add task2 and task3

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void task2(int a, int b) {
+        a = b - a;
+        b -= a;
+        a += b;
+
+        System.out.println();
+        System.out.println("Выходное значение a = " + a);
+        System.out.println("Выходное значение b = " + b);
+    }
+
+    public static boolean task3_2(String inputStr) {
+        String lowerCaseStr = inputStr.toLowerCase();
+        char[] charArray = lowerCaseStr.toCharArray();
+
+        int correctCounter = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == charArray[charArray.length - i - 1]) {
+                correctCounter++;
+            }
+        }
+
+        return correctCounter == charArray.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("===Task2===");
+        System.out.print("введите число а - ");
+
+        int a = scan.nextInt();
+        System.out.print("введите число b - ");
+        int b = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Входное значение а = " + a);
+        System.out.println("Входное значение b = " + b);
+
+        task2(a, b);
+        System.out.println("==========");
+        System.out.println();
+
+        System.out.println("===Task3===");
+        System.out.print("Введите строку - ");
+        String inputStr = scan.nextLine();
+
+        if (task3_2(inputStr)) {
+            System.out.println("Строка является палиндромом");
+        } else {
+            System.out.println("Строка не является палиндромом");
+        }
+
+        System.out.println("==========");
+    }
+}
